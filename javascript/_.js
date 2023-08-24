e2463b4fcdbb53c59f803a9fc1d1dfa8b8e1aaac
Aingell <skyleeb95@gmail.com>
2023-08-25 00:40:08 +0900

    2023.08.23 섹션2. 함수형으로 전환하기 - 6강 ~ 7강. 파이프라인, _go, _pipe, 화살표함수, 다형성 높이기, _keys, 에러 (다시 들어야함)

--- javascript/_.js
+++ javascript/_.js
@@ -22,8 +22,9 @@ function _map(list, mapper) {
     return new_list;
 }
 
+var _length = _get('length');
 function _each(list, iter) {
-    for(var i = 0; i < list.length; i++){
+    for(var i = 0, len = _length(list); i < len; i++) {
        iter(list[i]);
     }
     return list;
@@ -44,3 +45,31 @@ function _curryr(fn){
 var _get = _curryr(function (obj, key) {
     return obj == null ? undefined : obj[key];
 });
+
+function _rest(list, num) {
+    return slice.call(list, num || 1);
+}
+function _reduce(list, iter, memo) {
+    if(arguments.length == 2){
+        memo = list[0];
+        list = _rest(list);
+    }
+    _each(list, function(val){
+        memo = iter(memo, val);
+    });
+    return memo;
+}
+
+function _pipe(){
+    var fns = arguments;
+    return function (arg) {
+        return _reduce(fns, function (arg, fn) {
+            return fn(arg);
+        }, arg);
+    }
+}
+
+function _go(arg) {
+    var fns = _rest(arguments);
+    return _pipe.apply(null, fns)(arg);
+}
